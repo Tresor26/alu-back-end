ac4f2de80b4f532f6918df63bde09b4d9f4e2d27
Tresor26 <t.nkuruniz@alustudent.com>
2025-02-14 08:36:31 +0000

    documented the module

--- api/0-gather_data_from_an_API.py
+++ api/0-gather_data_from_an_API.py
@@ -1,37 +1,23 @@
 #!/usr/bin/python3
+
+"""gather data from an api"""
 import requests
 import sys
 
-def get_employee_todo_progress(employee_id):
-    base_url = "https://jsonplaceholder.typicode.com"
-    user_url = f"{base_url}/users/{employee_id}"
-    todos_url = f"{base_url}/todos?userId={employee_id}"
-    
-    user_response = requests.get(user_url)
-    todos_response = requests.get(todos_url)
-    
-    if user_response.status_code != 200 or todos_response.status_code != 200:
-        print("Error fetching data from API.")
-        return
-    
-    user_data = user_response.json()
-    todos_data = todos_response.json()
-    
-    employee_name = user_data.get("name")
-    total_tasks = len(todos_data)
-    completed_tasks = [task for task in todos_data if task.get("completed")]
-    num_completed = len(completed_tasks)
-    
-    print(f"Employee {employee_name} is done with tasks({num_completed}/{total_tasks}):")
-    for task in completed_tasks:
-        print(f"\t {task.get('title')}")
+if __name__ == '__main__':
+    """gather data from an api"""
+    response_todos = requests.get(
+        "https://jsonplaceholder.typicode.com/user/" + sys.argv[1] + "/todos"
+    )
+    data = response_todos.json()
+    completed_tasks = []
+    for i in data:
+        if i['completed']:
+            completed_tasks.append(i['title'])
 
-if __name__ == "__main__":
-    if len(sys.argv) != 2:
-        print("Usage: python3 0-gather_data_from_an_API.py <employee_id>")
-    else:
-        try:
-            employee_id = int(sys.argv[1])
-            get_employee_todo_progress(employee_id)
-        except ValueError:
-            print("Please enter a valid integer as employee ID.")
+    user = requests.get(
+        "https://jsonplaceholder.typicode.com/users/" + sys.argv[1]
+    ).json()
+    print("Employee {} is done with tasks({}/{}):".
+          format(user["name"], len(completed_tasks), len(data)))
+    [print("\t " + task) for task in completed_tasks]
